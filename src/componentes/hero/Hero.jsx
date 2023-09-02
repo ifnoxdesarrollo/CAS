978c6afb3d2cd04d9b64f813376a82b86fc65c1a
Evaristo.Aguirre <dasbrotchen@gmail.com>
2023-09-02 20:57:48 -0300

    faqs-prevencion-listo

--- src/componentes/hero/Hero.jsx
+++ src/componentes/hero/Hero.jsx
@@ -7,9 +7,9 @@ function Hero() {
     <div className="hero">
       <div className="hero__contenedor">
         <div className="hero__contenedor__encabezado">
-          <h3 className='hero__contenedor__encabezado__titulo-1'>Organizacion sin Fines de Lucro</h3>
-          <h1 className='hero__contenedor__encabezado__titulo-2'>Centro de Asistencia al Suicida</h1>
-          <h3 className='hero__contenedor__encabezado__titulo-3'>y Atención en Crisis</h3>
+          <h1 className='hero__contenedor__encabezado__titulo-1'>Centro de <br />Asistencia al <br />Suicida</h1>
+          <h3 className='hero__contenedo__encabezado__titulo-2'>y Atención en Crisis</h3>
+          <h3 className='hero__contenedor__encabezado__titulo-3'>Organizacion sin Fines de Lucro</h3>
         </div>
         <div className="hero__contenedor__botones">
           <h3 className='hero__contenedor__botones__titulo-1'>Atención únicamente Telefónica</h3>
