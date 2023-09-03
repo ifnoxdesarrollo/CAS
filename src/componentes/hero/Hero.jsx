da8757fcf5e0e01746f10e75876c960c040b8df2
Evaristo.Aguirre <dasbrotchen@gmail.com>
2023-09-02 21:31:59 -0300

    hero-retoques

--- src/componentes/hero/Hero.jsx
+++ src/componentes/hero/Hero.jsx
@@ -8,16 +8,17 @@ function Hero() {
       <div className="hero__contenedor">
         <div className="hero__contenedor__encabezado">
           <h1 className='hero__contenedor__encabezado__titulo-1'>Centro de <br />Asistencia al <br />Suicida</h1>
-          <h3 className='hero__contenedo__encabezado__titulo-2'>y Atención en Crisis</h3>
+          <h3 className='hero__contenedor__encabezado__titulo-2'>y Atención en Crisis</h3>
           <h3 className='hero__contenedor__encabezado__titulo-3'>Organizacion sin Fines de Lucro</h3>
         </div>
+
         <div className="hero__contenedor__botones">
           <h3 className='hero__contenedor__botones__titulo-1'>Atención únicamente Telefónica</h3>
           <a href='#'>
-            <button className='btn btn-lg hero__contenedor__botones__boton-1'>Llamar 0800</button>
+            <button className='boton hero__contenedor__botones__boton-1'>Llamar 0800</button>
           </a>
           <a href='#'>
-            <button className='btn btn-lg hero__contenedor__botones__boton-2'>Llamar por Whatsapp</button>
+            <button className='boton boton--verde hero__contenedor__botones__boton-2'>Llamar por Whatsapp</button>
           </a>
           <h3 className='hero__contenedor__botones__titulo-2'>Llamadas con costo</h3>
         </div>
