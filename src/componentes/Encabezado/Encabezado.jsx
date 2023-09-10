93f489eeaa112cf248cd9b923b3aa41618c33a52
Evaristo.Aguirre <dasbrotchen@gmail.com>
2023-09-10 11:48:05 -0300

    retoques-generales

--- src/componentes/Encabezado/Encabezado.jsx
+++ src/componentes/Encabezado/Encabezado.jsx
@@ -5,7 +5,7 @@ function Encabezado() {
 
   const [colorNav, setColorNav] = useState (false);
   const changeColorNav = () => {
-    if (window.scrollY >= 800) {
+    if (window.scrollY >= 780) {
       setColorNav (true)
     } else {
       setColorNav (false)
@@ -17,6 +17,7 @@ function Encabezado() {
   return (
     <>
       <header className={colorNav ? 'header-seccion header-seccion-scroll' : 'header-seccion'}>
+          {/* <a className="header-logo" href="#">CAS Mza</a>
           <nav className='navbar-seccion'>
                 <div className='navbar-seccion-menu'>
                     <li className='navbar-secciones-item'><a href=""></a>Historia del CAS</li>
@@ -29,9 +30,33 @@ function Encabezado() {
                     <span className="navbar-toggler-icon"></span>
                   </button>       
                 </div>
-          </nav>
+          </nav> */}
 
-          <a className="navbar-brand header-logo" href="#">CAS - Mza</a>
+          <nav className='navbar navbar-expand-md'>
+            <div className='container-fluid'>
+              {/* Logo*/}
+              <a className='navbar-brand' href="#">CAS Mza</a>
+
+              {/* boton hamburguesa*/}
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+              </button> 
+
+              {/* elementos del menu colapsable*/}
+              <div className="collapse navbar-collapse" id='menu'>
+                 <ul className='navbar-nav'>
+                  <li className='nav-itm'><a href=""></a>Historia del CAS</li>
+                  <li className='nav-itm'><a href=""></a></li>
+                  <li className='nav-itm'><a href=""></a></li>
+                 </ul>
+
+              </div>
+
+
+            </div>
+
+          </nav>
+          
 
       </header>
     
