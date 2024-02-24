4f1e309e9cb26f9a7eaed429c032526d3bb3e414
julieta548 <julietasosa548@gmail.com>
2024-02-24 09:22:15 -0300

    actualizacion febrero/marzo horarios

--- src/componentes/hero/Hero.jsx
+++ src/componentes/hero/Hero.jsx
@@ -33,11 +33,25 @@ class Hero extends Component {
 
   render() {
 
-    const feriado1 = new Date('2023-10-13');
-    const feriado2 = new Date('2023-10-16');
-    const feriado3 = new Date('2023-11-20');
-    const feriado4 = new Date('2023-12-05');
-    const feriado5 = new Date('2023-12-25');
+    const feriado1 = new Date('2024-03-24');
+    const feriado2 = new Date('2024-03-29');
+    const feriado3 = new Date('2024-04-01');
+    const feriado4 = new Date('2024-04-02');
+    const feriado5 = new Date('2024-05-01');
+    const feriado6 = new Date('2024-05-25');
+    const feriado7 = new Date('2024-06-17');
+    const feriado8 = new Date('2024-06-20');
+    const feriado9 = new Date('2024-06-21');
+    const feriado10 = new Date('2024-07-09');
+    const feriado11 = new Date('2024-10-11');
+    const feriado12 = new Date('2024-11-18');
+    const feriado13 = new Date('2024-12-08');
+    const feriado14 = new Date('2024-12-25');
+
+
+
+
+
 
     const dayOfWeek = new Date().getDay();
     const { currentTime } = this.state;
@@ -55,6 +69,15 @@ class Hero extends Component {
     const feriadoFormateado3 = formatearFeriado(feriado3);
     const feriadoFormateado4 = formatearFeriado(feriado4);
     const feriadoFormateado5 = formatearFeriado(feriado5);
+    const feriadoFormateado6 = formatearFeriado(feriado6);
+    const feriadoFormateado7 = formatearFeriado(feriado7);
+    const feriadoFormateado8 = formatearFeriado(feriado8);
+    const feriadoFormateado9 = formatearFeriado(feriado9);
+    const feriadoFormateado10 = formatearFeriado(feriado10);
+    const feriadoFormateado11 = formatearFeriado(feriado11);
+    const feriadoFormateado12 = formatearFeriado(feriado12);
+    const feriadoFormateado13 = formatearFeriado(feriado13);
+    const feriadoFormateado14 = formatearFeriado(feriado14);
 
     let modal = false;
 
@@ -77,32 +100,55 @@ class Hero extends Component {
       return `${año}-${mes}-${dia}`;
     }
 
-    if (dayOfWeek === 1 && currentHour > 14 && currentHour < 19) {
+    //hora de inicio debe ser la hora anterior a la de atención
+    //hora de cierre debe ser la misma hora de atención
+
+    if (dayOfWeek === 1 && currentHour > 8 && currentHour < 17) {
       content2 = "tel:08008000135";
       content = "https://wa.me/+5492615570314";
       hayAtencionWsp = true;
       hayAtencion0800 = true;
-    } else if (dayOfWeek === 2 && currentHour > 16 && currentHour < 21) {
+    } else if (dayOfWeek === 1 && currentHour > 18 && currentHour < 21) {
       content2 = "tel:08008000135";
       content = "https://wa.me/+5492615570314";
       hayAtencionWsp = true;
       hayAtencion0800 = true;
-    } else if (dayOfWeek === 3 && currentHour > 16 && currentHour < 19) {
+    } else if (dayOfWeek === 2 && currentHour > 12 && currentHour < 17) {
+      content2 = "tel:08008000135";
+      content = "https://wa.me/+5492615570314";
+      hayAtencionWsp = true;
+      hayAtencion0800 = true;
+    } else if (dayOfWeek === 2 && currentHour > 18 && currentHour < 21) {
+      content2 = "tel:08008000135";
+      content = "https://wa.me/+5492615570314";
+      hayAtencionWsp = true;
+      hayAtencion0800 = true;
+    } else if (dayOfWeek === 3 && currentHour > 8 && currentHour < 11) {
       content = "https://wa.me/+5492615570314";
       content2 = "tel:08008000135";
       hayAtencionWsp = true;
       hayAtencion0800 = true;
-    } else if (dayOfWeek === 4 && currentHour > 14 && currentHour < 19) {
+    } else if (dayOfWeek === 3 && currentHour > 14 && currentHour < 19) {
       content = "https://wa.me/+5492615570314";
       content2 = "tel:08008000135";
       hayAtencionWsp = true;
       hayAtencion0800 = true;
-    } else if (dayOfWeek === 5 && currentHour > 14 && currentHour < 17) {
+    } else if (dayOfWeek === 4 && currentHour > 10 && currentHour < 13) {
+      content2 = "tel:08008000135";
+      content = "https://wa.me/+5492615570314";
+      hayAtencionWsp = true;
+      hayAtencion0800 = true;
+    } else if (dayOfWeek === 4 && currentHour > 16 && currentHour < 19) {
       content = "https://wa.me/+5492615570314"
       content2 = "tel:08008000135";
       hayAtencionWsp = true;
       hayAtencion0800 = true;
-    } else if (dayOfWeek === 6 && currentHour > 17 && currentHour < 21) {
+    } else if (dayOfWeek === 5 && currentHour > 8 && currentHour < 11) {
+      content = "https://wa.me/+5492615570314";
+      content2 = "tel:08008000135";
+      hayAtencionWsp = true;
+      hayAtencion0800 = true;
+    } else if (dayOfWeek === 12 && currentHour > 8 && currentHour < 19) {
       content = "https://wa.me/+5492615570314";
       content2 = "tel:08008000135";
       hayAtencionWsp = true;
@@ -114,7 +160,12 @@ class Hero extends Component {
       hayAtencion0800 = false;
     } else if (fechaFormateada == feriadoFormateado1 ||
       fechaFormateada == feriadoFormateado2 || fechaFormateada == feriadoFormateado3 ||
-      fechaFormateada == feriadoFormateado4 || fechaFormateada == feriadoFormateado5 &&
+      fechaFormateada == feriadoFormateado4 || fechaFormateada == feriadoFormateado5 || 
+      fechaFormateada == feriadoFormateado6 || fechaFormateada == feriadoFormateado7 || 
+      fechaFormateada == feriadoFormateado8 || fechaFormateada == feriadoFormateado9 || 
+      fechaFormateada == feriadoFormateado10 || fechaFormateada == feriadoFormateado11 || 
+      fechaFormateada == feriadoFormateado12 || fechaFormateada == feriadoFormateado13 || 
+      fechaFormateada == feriadoFormateado14 &&
       (currentHour > 17 && currentHour < 21)) {
       content2 = "#";
       content = "https://wa.me/+5492622540949";
